cd7e91cd08306ebf72bac22610983766e1882c3f
nrbns357 <gusgh357@gmail.com>
2021-09-18 20:36:24 +0900

    upDate

--- src/CreateUser/index.js
+++ src/CreateUser/index.js
@@ -1,21 +1,9 @@
 import React from "react";
 
-function CreateUser({ username, email, onChange, onCreate }) {
+function CreateUser() {
   return (
     <div>
-      <input
-        name="username"
-        placeholder="계정명"
-        onChange={onChange}
-        value={username}
-      />
-      <input
-        name="email"
-        placeholder="이메일"
-        onChange={onChange}
-        value={email}
-      />
-      <button onClick={onCreate}>등록</button>
+      
     </div>
   );
 }
